9ee189d8bee06d8beec5d9190b2a24ae3114095a
Paulo Lopes <phlj.junior@gmail.com>
2022-01-06 13:15:51 -0300

    Promises

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -14,20 +14,64 @@ const criaNovaLinha = (nome, email) => {
     return linhaNovoCliente;
 }
 
+//Percorre o DOM
 const tabela = document.querySelector('[data-tabela]');
 
+const listaClientes = () => {
+
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+
+        http.open('GET', 'http://localhost:3000/profile');
 
 
-const http = new XMLHttpRequest();
 
-http.open('GET', 'http://localhost:3000/profile');
+        http.onload = () => {
+            if(http.status >= 400){
+                reject(JSON.parse(http.response));
+            } else {
+                resolve(JSON.parse(http.response));
+            }
+        }
 
-http.send();
-
-http.onload = () => {
-    const data = JSON.parse(http.response);
-
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+        http.send();
     });
+
+    console.log(promise);
+
+    return promise;
 }
+
+listaClientes()
+.then( data => {
+    data.forEach(elemento => {
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+    });
+})
+
+
+/*
+    Promise.all
+
+    Podemos fazer várias requisições que dependem uma da outra:
+
+    Ex:
+
+    movePersonagem(‘100’, ‘Esquerda’)
+    .then(() => movePersonagem(‘800’, ‘Direita’))
+    .then(() => movePersonagem(‘200’, ‘Esquerda’))
+    .then(() => movePersonagem(‘10’, ‘Direita’ ))
+    .then(() => movePersonagem(‘60’, ‘Esquerda’ ))
+
+    é o mesmo que
+
+    Promise.all([
+        movePersonagem(‘100’, ‘Esquerda’),
+        movePersonagem(‘800’, ‘Direita’),
+        movePersonagem(‘200’, ‘Esquerda’),
+        movePersonagem(10, ‘Esquerda’),
+        movePersonagem(‘60’, ‘Esquerda’)
+    ])
+    .then(...)
+
+*/
